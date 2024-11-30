d9fffba24a80e7729b750b3fd5186e41736ea34b
khoanisdev <doanvankhoan124@gmail.com>
2024-11-30 08:04:22 +0700

    Initial Commit

---
+++ TimePicker/TimePickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimePickerApp.swift
+//  TimePicker
+//
+//  Created by Đoàn Văn Khoan on 30/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TimePickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
